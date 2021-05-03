da57de75279f32471c16d896d005646058472b09
gerwld <47056812+gerwld@users.noreply.github.com>
2021-05-03 17:28:06 +0200

    Add icon changing when mobile menu is open or closed

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ let flexboxMenu = document.querySelector('.flexbox-menu');
 
 mobileMenuBtn.onclick = function(){
   flexboxMenu.classList.toggle('open');
+  if(flexboxMenu.classList.contains('open')) {
+    mobileMenuBtn.textContent = '× Menu';
+  } else mobileMenuBtn.textContent = '☰ Menu';
 }
